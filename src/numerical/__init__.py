b717aaeb5142500cf276d2f034bee7f43bc5e5da
Matthew Young <myoung.space.science@gmail.com>
2025-03-10 17:13:07 -0400

    Update __init__.py

--- src/numerical/__init__.py
+++ src/numerical/__init__.py
@@ -1,2 +1,30 @@
-def hello() -> str:
-    return "Hello from numerical!"
+from . import mixins
+from . import _operators as operators
+from ._protocols import (
+    Additive,
+    Algebraic,
+    Comparable,
+    Complex,
+    Multiplicative,
+    Orderable,
+    Real,
+    Sequence,
+    Value,
+)
+from ._types import Quantity
+
+
+__all__ = [
+    mixins,
+    operators,
+    Additive,
+    Algebraic,
+    Comparable,
+    Complex,
+    Multiplicative,
+    Orderable,
+    Quantity,
+    Real,
+    Sequence,
+    Value,
+]
