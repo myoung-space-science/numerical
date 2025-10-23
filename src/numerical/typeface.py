51899dd2d6943183aaabdc1736d8cf0bac3c9ba2
Matthew Young <myoung.space.science@gmail.com>
2025-10-23 13:09:23 -0400

    Update typeface.py
    * Refactor imports from `typing_extensions`
    * Edit module docstring

--- src/numerical/typeface.py
+++ src/numerical/typeface.py
@@ -4,23 +4,29 @@ Support for type annotations.
 This module provides a single interface to type annotations. For example,
 suppose `BestType` is available in the `typing` module starting with Python
 version 3.X, and is available in the `typing_extensions` module for earlier
-versions. This module will check the version of the active Python interpreter
-and will import from either `typing` or `typing_extensions` as appropriate, so
-that other modules may simply access `typehelp.BestType`.
+versions. If the user is running with Python version <3.X, this module will
+import `BestType` from `typing_extensions`. Otherwise, it will provide the
+implementation in `typing`.
 """
 
+import importlib
 from typing import *
-from typing_extensions import *
+
 
 __all__ = ()
 
 def __getattr__(name: str) -> type:
     """Get a built-in type annotation."""
     try:
-        attr = globals()[name]
-    except KeyError as err:
+        module = importlib.__import__(
+            'typing_extensions',
+            globals=globals(),
+            locals=locals(),
+            fromlist=[name],
+        )
+    except AttributeError as err:
         raise AttributeError(
             f"Could not find a type annotation for {name!r}"
         ) from err
-    return attr
+    return getattr(module, name)
 
